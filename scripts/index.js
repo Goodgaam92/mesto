95e0f4ba089f88c159bc43e711b22800ad24d318
Bekkhan Akhmedov <goodgaam95@gmail.com>
2022-08-11 21:40:31 +0300

    Исправил верстку и Js код

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,29 @@
-console.log('Hello>world');
 
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
+let inputJob = document.querySelector('.popup__input-job');
+let profileText = document.querySelector('.profile__text');
+let inputName = document.querySelector('.popup__input-name');
+let profileName = document.querySelector('.profile__name');
+let saveButton = document.querySelector('.popup__save-button');
+let form = document.querySelector('.form');
 
 let addPopup = function () {
     popup.classList.add('popup_opened');
 }
-
 let removePopup = function () {
     popup.classList.remove('popup_opened');
+}
+ function change (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileText.textContent = inputJob.value;
+    removePopup ();
 
-
+   
 }
 closeButton.addEventListener('click', removePopup);
 editButton.addEventListener('click', addPopup);
+form.addEventListener('submit', change);
 
-let inputName = document.querySelector('.popup__input-name');
-let profileName = document.querySelector('.profile__name');
-let saveButton = document.querySelector('.popup__save-button');
-
-let changeName = function () {
-    profileName.textContent = inputName.value;
-}
-
-let inputJob = document.querySelector('.popup__input-job');
-let profileText = document.querySelector('.profile__text');
-let changeText = function () {
-    profileText.textContent = inputJob.value;
-}
-
-saveButton.addEventListener('click', changeName,);
-saveButton.addEventListener('click', changeText);
