310807f6da583cf9bde869c17c1d6057c45db8bc
Bekkhan Akhmedov <goodgaam95@gmail.com>
2022-08-13 01:49:00 +0300

    next try

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
-let inputJob = document.querySelector('.popup__input-job');
+let inputJob = document.querySelector('.popup__input_type_job');
 let profileText = document.querySelector('.profile__text');
-let inputName = document.querySelector('.popup__input-name');
+let inputName = document.querySelector('.popup__input_type_name');
 let profileName = document.querySelector('.profile__name');
 let saveButton = document.querySelector('.popup__save-button');
-let form = document.querySelector('.form');
+let form = document.querySelector('.popup__form');
 
 let addPopup = function () {
     popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputJob.value = profileText.textContent;
 }
 let removePopup = function () {
     popup.classList.remove('popup_opened');
